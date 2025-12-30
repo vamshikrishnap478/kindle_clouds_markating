38538a4c32b02c9e5ef442a2229839939ee3faaf
vamshikrishnap478 <vk480606@gmail.com>
2025-12-30 21:23:16 +0530

    Site Under Maintenance

--- Kindleclouds/src/Compounents/Pages/Home.tsx
+++ Kindleclouds/src/Compounents/Pages/Home.tsx
@@ -3,7 +3,7 @@ import react from "react"
 const home : react.FC  = () => {
     return (
         <><header></header>
-        <div><h1>Comming Soon</h1></div>
+        <div><h1>Site Under Maintenance.</h1></div>
         </>
     )
 }
